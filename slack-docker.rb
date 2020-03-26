e981c065867af6c06eb0add15cc8348cfcea6aca
Hidetake Iwata <int128@gmail.com>
2020-03-26 10:38:44 +0900

    Bump the version to v2.0.2 (#1)

--- slack-docker.rb
+++ slack-docker.rb
@@ -1,9 +1,9 @@
 class SlackDocker < Formula
   desc "Slack integration to notify Docker events"
   homepage "https://github.com/int128/slack-docker"
-  url "https://github.com/int128/slack-docker/releases/download/2.0.1/slack-docker_2.0.1_darwin_amd64.tar.gz"
-  version "2.0.1"
-  sha256 "a14044a8119dd28587271e8715f2144f04ab6a30c374dcb055dda924eed44d63"
+  url "https://github.com/int128/slack-docker/releases/download/v2.0.2/slack-docker_darwin_amd64.zip"
+  version "v2.0.2"
+  sha256 "842f556f90c731d94ddd9e2b3111fc578505352cb584ff195a8ea755ca5333c7"
 
   def install
     bin.install "slack-docker"
