08a17243c6daa419eed17793216e9e1498399854
Rob Mills <robgmills@users.noreply.github.com>
2021-02-16 14:51:15 -0600

    Bump the version to beta

--- slack-docker.rb
+++ slack-docker.rb
@@ -3,7 +3,7 @@ class SlackDocker < Formula
   homepage "https://github.com/int128/slack-docker"
   url "https://github.com/int128/slack-docker/releases/download/beta/slack-docker_darwin_amd64.zip"
   version "beta"
-  sha256 "d52fdeb5b8ac545ec3f14a271354788d9b05f018e6a56c40633c6a7652e7693c"
+  sha256 "41aac52343bc843e614f26b96caf0467e0cb0097b4ce642fb8fdedd9d6c8f9b8"
 
   def install
     bin.install "slack-docker"
